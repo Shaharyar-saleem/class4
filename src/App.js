ca3f78c11f781eebe7e745870808bb7e39d864f6
Shaharyar-saleem <43236957+Shaharyar-saleem@users.noreply.github.com>
2020-06-14 18:39:20 +0500

    build the project

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
         counter >= 5 ? "semi-danger" : "body"
       } ${counter >= 7 ? "danger" : " "} `}
     >
+      <h1>hi this is the msg</h1>
       <Message count={counter} />
       <button onClick={() => setCounter(++counter)}>Add value</button>
       <button onClick={() => setCounter(0)}>Reset</button>
