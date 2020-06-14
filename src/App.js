491abacf754e48cdbeac9b28b4b086491e0a9e7f
Shaharyar-saleem <43236957+Shaharyar-saleem@users.noreply.github.com>
2020-06-14 19:42:24 +0500

    update in the app.js file

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
         counter >= 7 ? "danger" : " "
       } `}
     >
-      <h1>hi this is the {counter=<5 ? 'Safe State' : 'Danger State'}</h1>
+      <h1>hi this is the {counter <= 5 ? "Safe State" : "Danger State"}</h1>
       <Message count={counter} />
       <button onClick={() => setCounter(++counter)}>Add value</button>
       <button onClick={() => setCounter(0)}>Reset</button>
